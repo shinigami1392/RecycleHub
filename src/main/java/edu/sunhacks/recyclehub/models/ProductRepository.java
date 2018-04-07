dcd3d4252c1bf8fd9af358306c209f09d11befe5
shimigami1392 <pushkar.ladhe@asu.edu>
2018-04-07 09:32:33 -0700

    Submitting REST Repository interface using spring rest to access MongoDB data

--- src/main/java/edu/sunhacks/recyclehub/models/ProductRepository.java
+++ src/main/java/edu/sunhacks/recyclehub/models/ProductRepository.java
@@ -1,9 +1,10 @@
 package edu.sunhacks.recyclehub.models;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
-public interface ProductRepository extends MongoRepository{
+@RepositoryRestResource(collectionResourceRel = "product", path = "product")
+public interface ProductRepository extends MongoRepository<Product, String>{
 	public Product findByName(String name);
 }
