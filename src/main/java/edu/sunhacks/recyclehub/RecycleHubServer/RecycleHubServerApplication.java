9cda5a5e3cb48061453245b75773a43038fc6787
shimigami1392 <pushkar.ladhe@asu.edu>
2018-04-07 01:35:03 -0700

    First Commit

---
+++ src/main/java/edu/sunhacks/recyclehub/RecycleHubServer/RecycleHubServerApplication.java
@@ -0,0 +1,12 @@
+package edu.sunhacks.recyclehub.RecycleHubServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecycleHubServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecycleHubServerApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/sunhacks/recyclehub/RecycleHubServer/RecycleHubServerApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.sunhacks.recyclehub.RecycleHubServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecycleHubServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
