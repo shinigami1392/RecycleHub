ca4f41f6e6e7e203106c5511886148fe9c916c46
shimigami1392 <pushkar.ladhe@asu.edu>
2018-04-07 13:35:17 -0700

    Submitting REST controller for product collection

--- src/main/java/edu/sunhacks/recyclehub/controller/ProductController.java
+++ src/main/java/edu/sunhacks/recyclehub/controller/ProductController.java
@@ -1,13 +1,31 @@
 package edu.sunhacks.recyclehub.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.sunhacks.recyclehub.models.Product;
 import edu.sunhacks.recyclehub.models.ProductRepository;
 
-
+@RestController
+@RequestMapping("/products")
 public class ProductController {
 	
+	private ProductRepository productRepository;
+
+	public ProductRepository getProductRepository() {
+		return productRepository;
+	}
+
+	public void setProductRepository(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+	
+	@GetMapping("/findByName")
+	public List<Product> findByName(List<String> names){
+		return productRepository.findByName(names);
+	}
 
 }
--- src/main/java/edu/sunhacks/recyclehub/models/ProductRepository.java
+++ src/main/java/edu/sunhacks/recyclehub/models/ProductRepository.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.transaction.annotation.Transactional;
 
-@RepositoryRestResource(collectionResourceRel = "product", path = "product")
+
 public interface ProductRepository extends MongoRepository<Product, String>{
 	public List<Product> findByName(@Param("name") List<String> name);
 }
